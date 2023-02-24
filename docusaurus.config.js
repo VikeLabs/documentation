78020d039aad3d044aad83c69ad9be3708cc65a2
robchendev <robchendev@gmail.com>
2023-02-24 14:35:28 -0800

    removed branding for a clean slate

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,9 +29,9 @@ const config = {
         blog: {
           showReadingTime: true,
         },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
+        // theme: {
+        //   customCss: require.resolve('./src/css/custom.css'),
+        // },
       }),
     ],
   ],
@@ -42,9 +42,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Docs',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'VikeLabs Logo',
           src: 'img/logo.svg',
         },
         items: [
@@ -68,28 +68,19 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
             title: 'Community',
             items: [
               {
                 label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                href: '#',
               },
               {
                 label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                href: '#',
               },
               {
                 label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                href: '#',
               },
             ],
           },
@@ -97,17 +88,12 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: '#',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
